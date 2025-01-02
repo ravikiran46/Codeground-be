7331ced020160418d33ad16c2c02c269795c2b27
ravikran4 <kiran04092003@gmail.com>
2025-01-02 10:02:05 +0530

    Revert "fix!: response message"
    This reverts commit fd925e8b40a8f69363d78d7029c8ac4e16b164db.

--- controller/user.js
+++ controller/user.js
@@ -42,7 +42,7 @@ const login = async (req, res) => {
     });
 
     if (!user) {
-      return res.status(404).json({ message: "User not found, SignUp" });
+      return res.status(404).json({ message: "No user Found" });
     }
 
     const isMatch = await bcrypt.compare(password, user.password);
