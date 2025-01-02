a4e54a28c1dab12a057eaf0f5fa68139ad1f1723
ravikran4 <kiran04092003@gmail.com>
2025-01-02 10:11:59 +0530

    fix!: savecode not working

--- controller/user_code.js
+++ controller/user_code.js
@@ -1,11 +1,6 @@
 const Codes = require("../model/codes");
-const dbconnect = require("./utils/dbconnection");
-
 const UserCodes = async (req, res) => {
   try {
-    dbconnect(dburi)
-      .then(() => console.log("connected to db"))
-      .catch(() => console.log("cannot connect to db"));
     const savedcodes = await Codes.find({ user_Id: req.user.id });
     if (!savedcodes) res.status(404).json({ message: "No saved codes" });
     res.status(200).json({ codes: savedcodes });
@@ -19,9 +14,6 @@ const UserCodes = async (req, res) => {
 const getcode = async (req, res) => {
   const id = req.query.id;
   try {
-    dbconnect(dburi)
-      .then(() => console.log("connected to db"))
-      .catch(() => console.log("cannot connect to db"));
     const code = await Codes.findOne({ _id: id });
     if (!code) {
       return res.status(404).json({ message: "code doesn't exists" });
@@ -36,9 +28,6 @@ const getcode = async (req, res) => {
 const savecode = async (req, res) => {
   const { title, code, lang } = req.body;
   try {
-    dbconnect(dburi)
-      .then(() => console.log("connected to db"))
-      .catch(() => console.log("cannot connect to db"));
     const titleexist = await Codes.findOne({ title: title });
     console.log(titleexist);
     if (titleexist) return res.status(409).json({ message: "Use new title" });
@@ -61,9 +50,6 @@ const updatecode = async (req, res) => {
   const id = req.query.id;
   const { title, code, lang } = req.body;
   try {
-    dbconnect(dburi)
-      .then(() => console.log("connected to db"))
-      .catch(() => console.log("cannot connect to db"));
     const codeexist = await Codes.findById(id);
     if (codeexist) {
       codeexist.title = title || codeexist.title;
